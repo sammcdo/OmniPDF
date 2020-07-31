ea824ef29c3780987c6b91f8f0ccaf7ae90ed13c
Sam <sam_mcdo@yahoo.com>
2020-07-31 09:45:52 -0700

    added menu, no actions

--- GUI.py
+++ GUI.py
@@ -2,7 +2,8 @@ import PySimpleGUI as sg
 
 import PdfHandler
 
-class GUI():
+
+class GUI:
     def __init__(self, filename):
         self.pdf = PdfHandler.PdfHandler(filename)
 
@@ -13,8 +14,16 @@ class GUI():
         self.image_elem = sg.Image(data=data)
 
         self.goto = sg.InputText(str(self.cur_page + 1), size=(5, 1), key="-PageNumber-")
+
+        self.menu_def = [['&File', ['&Open     Ctrl-O', '&Save       Ctrl-S', '&Properties', 'E&xit']],
+                    ['&Edit', ['&Paste', ['Special', 'Normal', ], 'Undo'], ],
+                    ['&Toolbar', ['---', 'Command &1', 'Command &2',
+                                  '---', 'Command &3', 'Command &4']],
+                    ['&Help', '&About...'], ]
         
-        self.layout = [[
+        self.layout = [
+            [sg.Menu(self.menu_def, tearoff=False, pad=(200, 1))],
+            [
                 sg.ReadButton('Next'),
                 sg.ReadButton('Prev'),
                 sg.Text('Page:'),
@@ -26,7 +35,7 @@ class GUI():
 
         title = "OmniPDF"
         self.window = sg.Window(title, self.layout, return_keyboard_events=True,
-                   location=(0, 0), use_default_focus=False, no_titlebar=False)
+                                location=(0, 0), use_default_focus=False, no_titlebar=False)
 
         # now define the buttons / events we want to handle
         self.enter_buttons = [chr(13), "Return:13"]
@@ -36,15 +45,16 @@ class GUI():
 
         # all the buttons we will handle
         self.my_keys = self.enter_buttons + self.next_buttons + self.prev_buttons
-        
+
+    def onAbout(self):
+        self.window.disappear()
+        sg.popup('About this program', 'Version 1.0',
+                 'PySimpleGUI Version', sg.version, grab_anywhere=True)
+        self.window.reappear()
 
     def mainloop(self):
         while True:
             event, value = self.window.read()
-            if event == sg.WIN_CLOSED and (value is None or value['-PageNumber-'] is None):
-                break
-            if event in self.quit_buttons:
-                break
 
             if event in self.enter_buttons:
                 try:
@@ -71,5 +81,15 @@ class GUI():
             # update page number field
             if event in self.my_keys:
                 self.goto.update(str(self.cur_page + 1))
+
+            """ --- Menu Events --- """
+            if event == "About...":
+                self.onAbout()
+
+            """ --- Exit Events --- """
+            if event == sg.WIN_CLOSED and (value is None or value['-PageNumber-'] is None):
+                break
+            if event in self.quit_buttons:
+                break
 
         self.window.close()
--- PdfHandler.py
+++ PdfHandler.py
@@ -5,6 +5,8 @@ class PdfHandler():
         self.pdf = fitz.open(filename)
 
         self.pages = [None] * len(self.pdf)
+        for pageNum in range(len(self.pdf)):
+            self.pages[pageNum] = self.pdf[pageNum].getDisplayList()
 
     def getImage(self, pageNum = 0):
         page = self.pages[pageNum]
@@ -16,3 +18,14 @@ class PdfHandler():
         pixMap = page.getPixmap(matrix=mat_0, alpha=False)
 
         return pixMap.getImageData("ppm")
+
+    def savePageAsImage(self, filename, pageNum=0):
+        page = self.pages[pageNum]
+        if page == None:
+            self.pages[pageNum] = self.pdf[pageNum].getDisplayList()
+            page = self.pages[pageNum]
+
+        mat_0 = fitz.Matrix(1, 1)
+        pixMap = page.getPixmap(matrix=mat_0, alpha=False)
+
+        pixMap.writePNG(filename)
